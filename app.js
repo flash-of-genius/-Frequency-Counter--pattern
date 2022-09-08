133c6fa674a47e7e1bf04d71dbab88b833608417
flash-of-genius <amhdaar@gmail.com>
2022-09-08 12:05:20 +0100

    Getting started

---
+++ app.js
@@ -0,0 +1,5 @@
+// First, using the function keyword, we create a function with the identifier sameSquared
+
+// Our function sameSquared needs two parameters, a first array and a second array.
+
+function sameSquared(firstArr, secondArr) {}
